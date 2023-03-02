ebbd78064b2a5d6aa5b69303035c8aeeb321c76f
jrh <joshuaharris1103@gmail.com>
2023-03-02 16:49:09 -0500

    Part 1 done

---
+++ main_app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+
+finches = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+]
+
+
+
+def home(request):
+  return render(request, 'home.html')
+  
+def about(request):
+  return render(request, 'about.html')
+
+def finch_index(request):
+  return render(request, 'finches/index.html', {
+    'finches': finches
+  })

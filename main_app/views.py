76136fa47983904916837287348b7b7e4bc8be3b
jrh <joshuaharris1103@gmail.com>
2023-03-03 14:36:01 -0500

    Part 2 & Part 3 Done

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Finch
 
 
 finches = [
   {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
   {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
 ]
-
 
 
 def home(request):
@@ -15,6 +16,21 @@ def about(request):
   return render(request, 'about.html')
 
 def finch_index(request):
-  return render(request, 'finches/index.html', {
-    'finches': finches
-  })
+  finches = Finch.objects.all()
+  return render(request, 'finches/index.html', { 'finches': finches })
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', { 'finch': finch })
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches/'
